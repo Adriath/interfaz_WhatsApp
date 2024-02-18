0ebc0395a699bae465cf3287dac232ad2eeba852
Adriath <adrianarjonabravo@gmail.com>
2024-02-18 21:42:21 +0100

    En este punto ya funciona al pulsar sobre la cámara. De momento vuelca un mensaje provisional, ahora hay que sustituir por la Activity.

--- app/src/main/java/com/example/interfazwhatsapp/MainActivity.java
+++ app/src/main/java/com/example/interfazwhatsapp/MainActivity.java
@@ -3,15 +3,27 @@ package com.example.interfazwhatsapp;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    MenuItem itCamara ;
+    MenuItem itBusqueda ;
+    MenuItem itNuevoGrupo ;
+    MenuItem itNuevaDifusion ;
+    MenuItem itAjustes ;
+
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        itCamara =findViewById(R.id.Camara) ;
 
         if(getSupportActionBar() != null){
             getSupportActionBar().show() ;
@@ -26,6 +38,20 @@ public class MainActivity extends AppCompatActivity {
         return true ;
     }
 
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item){
 
+        int opcion = item.getItemId() ;
+
+        if(opcion == R.id.Camara){
+
+            Toast.makeText(this,"Todo va bien", Toast.LENGTH_SHORT).show() ;
+            return true ;
+        }
+        else{
+
+            return super.onOptionsItemSelected(item) ;
+        }
+    }
 
 }
